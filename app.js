f4144ed682ec4ee67a7244a62b7d12539e6e5ad6
NamorAgro <99247160+NamorAgro@users.noreply.github.com>
2023-08-14 12:54:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-function updateLayers(x, y) {
+window.addEventListener('deviceorientation', function(event) {
+    console.log("Beta: " + event.beta);
+    console.log("Gamma: " + event.gamma);
+  
+    let x = event.gamma; // In degree in the range [-180,180]
+    let y = event.beta; // In degree in the range [-90,90]
+  
     Object.assign(document.documentElement.style, {
-      '--move-x': x + 'deg',
-      '--move-y': y + 'deg',
+      '--move-x': x * -.005 + 'deg',
+      '--move-y': y * -.01 + 'deg',
     });
-  }
-  
-  // Handle desktop mouse movement
-  document.addEventListener('mousemove', e => {
-    updateLayers((e.clientX - window.innerWidth / 2) * -.005, (e.clientY - window.innerHeight / 2) * -.01);
   });
+// function updateLayers(x, y) {
+//     Object.assign(document.documentElement.style, {
+//       '--move-x': x + 'deg',
+//       '--move-y': y + 'deg',
+//     });
+//   }
   
-  // Handle mobile device orientation
-  if (window.DeviceOrientationEvent) {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      // iOS 13+
-      document.addEventListener('touchstart', () => {
-        DeviceOrientationEvent.requestPermission()
-          .then(response => {
-            if (response === 'granted') {
-              window.addEventListener('deviceorientation', e => {
-                updateLayers(e.gamma * -.005, e.beta * -.01);
-              });
-            }
-          })
-          .catch(console.error);
-      });
-    } else {
-      // non iOS 13+
-      window.addEventListener('deviceorientation', e => {
-        updateLayers(e.gamma * -.005, e.beta * -.01);
-      });
-    }
-  }
+//   // Handle desktop mouse movement
+//   document.addEventListener('mousemove', e => {
+//     updateLayers((e.clientX - window.innerWidth / 2) * -.005, (e.clientY - window.innerHeight / 2) * -.01);
+//   });
+  
+//   // Handle mobile device orientation
+//   if (window.DeviceOrientationEvent) {
+//     if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+//       // iOS 13+
+//       document.addEventListener('touchstart', () => {
+//         DeviceOrientationEvent.requestPermission()
+//           .then(response => {
+//             if (response === 'granted') {
+//               window.addEventListener('deviceorientation', e => {
+//                 updateLayers(e.gamma * -.005, e.beta * -.01);
+//               });
+//             }
+//           })
+//           .catch(console.error);
+//       });
+//     } else {
+//       // non iOS 13+
+//       window.addEventListener('deviceorientation', e => {
+//         updateLayers(e.gamma * -.005, e.beta * -.01);
+//       });
+//     }
+//   }
