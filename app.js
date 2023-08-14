5d3b8471dfc6c3ddad64ab2cd4a6c3d4efac79ab
NamorAgro <99247160+NamorAgro@users.noreply.github.com>
2023-08-14 16:25:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// document.addEventListener('mousemove', e => {
-//     Object.assign(document.documentElement, {
-//       style: `
-//         --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-//         --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
-//       `
-//     });
-//   });
-// report_box = document.getElementById('report_box')
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+      style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+      `
+    });
+  });
+report_box = document.getElementById('report_box')
 
 // let sensor = new Gyroscope();
 // let x, y, z, report;
@@ -27,20 +27,19 @@
 //     }
 // };
 
-
-function handleOrientation(event) {
-    var beta = event.beta; // In degree in the range [-180,180]
-    var gamma = event.gamma; // In degree in the range [-90,90]
+// function handleOrientation(event) {
+//     var beta = event.beta; // In degree in the range [-180,180]
+//     var gamma = event.gamma; // In degree in the range [-90,90]
   
-    // Update the content of the div with the orientation data
-    document.getElementById('orientationData').innerHTML = 'Beta: ' + beta + '<br>Gamma: ' + gamma;
-  }
+//     // Update the content of the div with the orientation data
+//     document.getElementById('orientationData').innerHTML = 'Beta: ' + beta + '<br>Gamma: ' + gamma;
+//   }
   
-  // Add event listener for device orientation
-  if (window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', handleOrientation);
-  } else {
-    document.getElementById('orientationData').innerHTML = 'Sorry, your browser doesn\'t support Device Orientation';
-  }
+//   // Add event listener for device orientation
+//   if (window.DeviceOrientationEvent) {
+//     window.addEventListener('deviceorientation', handleOrientation);
+//   } else {
+//     document.getElementById('orientationData').innerHTML = 'Sorry, your browser doesn\'t support Device Orientation';
+//   }
 
 
