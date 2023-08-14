73f561f378f1fe850bce1cd5fde3ee4e241aae58
NamorAgro <99247160+NamorAgro@users.noreply.github.com>
2023-08-14 12:58:58 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-window.addEventListener('deviceorientation', function(event) {
+document.getElementById('requestPermission').addEventListener('click', function() {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission()
+        .then(response => {
+          if (response === 'granted') {
+            window.addEventListener('deviceorientation', handleOrientation);
+          }
+        })
+        .catch(console.error);
+    } else {
+      // For non-iOS 13 devices
+      window.addEventListener('deviceorientation', handleOrientation);
+    }
+  });
+  
+  function handleOrientation(event) {
     console.log("Beta: " + event.beta);
     console.log("Gamma: " + event.gamma);
   
     let x = event.gamma; // In degree in the range [-180,180]
-    let y = event.beta; // In degree in the range [-90,90]
+    let y = event.beta;  // In degree in the range [-90,90]
   
     Object.assign(document.documentElement.style, {
       '--move-x': x * -.005 + 'deg',
       '--move-y': y * -.01 + 'deg',
     });
-  });
+  }
 // function updateLayers(x, y) {
 //     Object.assign(document.documentElement.style, {
 //       '--move-x': x + 'deg',
